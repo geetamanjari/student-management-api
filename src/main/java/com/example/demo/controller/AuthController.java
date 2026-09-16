1d99e0487125f2281f661f9bde003d156f28b10d
geetamanjari <joneboinageetamanjari@gmail.com>
2026-09-16 15:46:48 +0530

    Added Swagger Documentation

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -2,11 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.User;
 import com.example.demo.repo.UserRepository;
-import com.example.demo.security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.*;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,22 +15,10 @@ public class AuthController {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private JwtUtil jwtUtil;
-
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
     @PostMapping("/register")
     public ResponseEntity<Map<String, String>>
     register(@RequestBody User user) {
 
-        user.setPassword(
-                passwordEncoder.encode(user.getPassword()));
-        user.setRole("USER");
         userRepository.save(user);
 
         Map<String, String> response = new HashMap<>();
@@ -46,15 +31,8 @@ public class AuthController {
     public ResponseEntity<Map<String, String>>
     login(@RequestBody User user) {
 
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        user.getUsername(), user.getPassword()));
-
-        String token = jwtUtil
-                .generateToken(user.getUsername());
-
         Map<String, String> response = new HashMap<>();
-        response.put("token", token);
+        response.put("message", "Login successful");
         return ResponseEntity.ok(response);
     }
 }
